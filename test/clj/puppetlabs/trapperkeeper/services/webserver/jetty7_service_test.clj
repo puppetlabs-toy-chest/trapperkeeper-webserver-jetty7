0bb23e631664e9d0c5425b717c8cfa7d54194097
Chris Price <chris@puppetlabs.com>
2014-01-30 17:03:51 -0800

    Remove unneccessary joins from tests

--- test/clj/puppetlabs/trapperkeeper/services/webserver/jetty7_service_test.clj
+++ test/clj/puppetlabs/trapperkeeper/services/webserver/jetty7_service_test.clj
@@ -16,7 +16,6 @@
     (let [app              (bootstrap-services-with-cli-data [jetty7-service] {:config "./test-resources/config/jetty/jetty.ini"})
           s                (get-service app :WebserverService)
           add-ring-handler (partial add-ring-handler s)
-          join             (partial join s)
           shutdown         (partial stop s (service-context s))
           body             "Hello World"
           path             "/hello_world"
@@ -24,7 +23,6 @@
       (try
         (add-ring-handler ring-handler path)
         ;; Spin up jetty in a separate thread
-        (future (join))
         ;; host and port are defined in config file used above
         (let [response (http-client/get (format "http://localhost:8080/%s/" path))]
           (is (= (response :status) 200))
@@ -36,14 +34,12 @@
     (let [app                 (bootstrap-services-with-empty-config [jetty7-service])
           s                   (get-service app :WebserverService)
           add-servlet-handler (partial add-servlet-handler s)
-          join                (partial join s)
           shutdown            (partial stop s (service-context s))
           body                "Hey there"
           path                "/hey"
           servlet             (SimpleServlet. body)]
       (try
         (add-servlet-handler servlet path)
-        (future (join))
         (let [response (http-client/get (format "http://localhost:8080/%s" path))]
           (is (= (:status response) 200))
           (is (= (:body response) body)))
@@ -56,14 +52,12 @@
       (let [app               (bootstrap-services-with-cli-data [jetty7-service] {:config config})
             s                 (get-service app :WebserverService)
             add-ring-handler  (partial add-ring-handler s)
-            join              (partial join s)
             shutdown          (partial stop s (service-context s))
             body              "Hi World"
             path              "/hi_world"
             ring-handler      (fn [req] {:status 200 :body body})]
         (try
           (add-ring-handler ring-handler path)
-          (future (join))
           ;; NOTE that we're not entirely testing SSL here since we're not hitting https 8081
           ;; but this at least tests the initialization. Unfortunately when you are using a
           ;; self-signed certificate on the server it's really hard to do a client request
